2439c6470f3a19102847dada9d0f61c95dc5d582
Miguel Fernandez <miguelferlez@gmail.com>
2025-04-13 13:43:55 +0200

    refactor:  hamburger menu controller and drop down style

---
+++ components/hamburger-menu/hamburgerMenuController.js
@@ -0,0 +1,4 @@
+export const toggleMenu = (button, menu) => {
+    button.classList.toggle('active');
+    menu.classList.toggle('active');
+};
--- index.js
+++ index.js
@@ -1,12 +1,8 @@
-
+import { toggleMenu } from "./components/hamburger-menu/hamburgerMenuController.js";
 
 document.addEventListener('DOMContentLoaded', () => {
     const hamburger = document.querySelector('.hamburger');
     const navMenu = document.querySelector('.nav-menu');
-    const toggleMenu = () => {
-        hamburger.classList.toggle('active');
-        navMenu.classList.toggle('active');
-    };
-    hamburger.addEventListener('click', toggleMenu);
+    hamburger.addEventListener('click', () => { toggleMenu(hamburger, navMenu) });
 
 });
