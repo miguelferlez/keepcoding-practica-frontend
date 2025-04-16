c30f76a64c56cfab3858522f7379a28b888d474c
Miguel Fernandez <miguelferlez@gmail.com>
2025-04-16 13:24:33 +0200

    style: floating notification with fade out remove

--- components/notifications/notificationsController.js
+++ components/notifications/notificationsController.js
@@ -5,20 +5,28 @@ export function showNotifications(container) {
         notification.remove();
     };
 
+    const fadeOut = (duration, notification) => {
+        notification.classList.add("fade-out");
+        setTimeout(() => {
+            remove(notification);
+        }, duration);
+
+    }
+    
     const notify = (message, type = "error") => {
         const notification = document.createElement("div");
         notification.classList.add("notification");
         notification.classList.add(type);
         notification.innerHTML = notificationElement(message);
         container.appendChild(notification);
-
+        
         const closeButton = notification.querySelector("button");
         closeButton.addEventListener("click", () => {
             remove(notification);
         });
-
+        
         setTimeout(() => {
-            remove(notification);
+            fadeOut(1000, notification);
         }, 5000);
     };
 
